527fc639d32bc7b1ccb3b8925a93f6ac5996f628
Akhil-virja <virjaakhil555@gmail.com>
2025-07-12 11:30:19 +0530

    base from my side

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const dbInstance = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log("MongoDB connected");
+    console.log(`MongoDB connected ${dbInstance.connection.host}`);
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
     process.exit(1);
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
-require('dotenv').config(); // ✅ first line
+require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./db/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
